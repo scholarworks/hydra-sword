d95b4fe46dd39d563142b0496494b84cad845040
Aaron Collier <acollier@calstate.edu>
2017-02-17 17:17:37 -0800

    Add route generator

---
+++ lib/generators/sword/sword_generator.rb
@@ -0,0 +1,9 @@
+# -*- encoding : utf-8 -*-
+require 'rails/generators'
+
+class SwordGenerator < Rails::Generators::Base
+  def inject_routes
+  	routing_code = "mount Hydra::Sword:Engine => '/'"
+  	inject_into_file 'config/routes.rb', "\n #{routing_code}\n", {:after => "# API routes\n", :verbose => false}
+  end
+end
