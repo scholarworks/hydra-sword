df6d23f474b669c43bd12f97b3c9ad07ca453670
Aaron Collier <acollier@calstate.edu>
2017-02-17 17:26:54 -0800

    Typo in route generator

--- lib/generators/sword/sword_generator.rb
+++ lib/generators/sword/sword_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators'
 
 class SwordGenerator < Rails::Generators::Base
   def inject_routes
-  	routing_code = "mount Hydra::Sword:Engine => '/'"
+  	routing_code = "mount Hydra::Sword::Engine => '/'"
   	inject_into_file 'config/routes.rb', "\n #{routing_code}\n", {:after => "# API routes\n", :verbose => false}
   end
 end
